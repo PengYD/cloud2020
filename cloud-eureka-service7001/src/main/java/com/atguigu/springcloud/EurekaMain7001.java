9ac9b38a39471abec71c8b9502fa1ea710868df2
PengYD <pengyd@178542>
2020-07-16 17:09:21 +0800

    Euerka

--- cloud-eureka-service7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
+++ cloud-eureka-service7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -1,23 +1,20 @@
 package com.atguigu.springcloud;
 
-import javafx.application.Application;
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author : PengYanDong
  * @description : ${description}
- * @create : 2020-07-16 10:27
+ * @create : 2020-07-16 14:20
  * <p>
  * Copyright 2020 All rights reserved.
  **/
 @SpringBootApplication
-@EnableEurekaClient
-public class OrderMain80 {
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(OrderMain80.class,args);
+        SpringApplication.run(EurekaMain7001.class,args);
     }
-
 }
