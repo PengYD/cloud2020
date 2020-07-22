b3f66979634f464bcbeefc97634073fbac33c426
PengYD <pengyd@178542>
2020-07-22 14:21:45 +0800

    配置中心

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/Controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/Controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  * <p>
  * Copyright 2020 All rights reserved.
  **/
+@RefreshScope
 @RestController
 public class ConfigClientController {
 
