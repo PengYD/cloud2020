dd9b42223c4a07b9dfa93ffc64225638e77fd547
PengYD <pengyd@178542>
2020-07-25 17:21:05 +0800

    利用分组，解决消息重复消费

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamMQMain8803.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : PengYanDong
+ * @description : ${description}
+ * @create : 2020-07-22 15:56
+ * <p>
+ * Copyright 2020 All rights reserved.
+ **/
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class, args);
+    }
+}
