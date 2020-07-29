fbc6f741c9e18c1252991fcc54af76b89149a26b
PengYD <pengyd@178542>
2020-07-29 18:20:45 +0800

    nacos配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author : PengYanDong
+ * @description : ${description}
+ * @create : 2020-07-29 16:59
+ * <p>
+ * Copyright 2020 All rights reserved.
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
