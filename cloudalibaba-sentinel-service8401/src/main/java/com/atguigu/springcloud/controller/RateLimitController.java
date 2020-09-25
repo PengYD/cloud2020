7d8e708a9a09f962b79151837e0765093b83e1f8
PengYD <pengyd@178542>
2020-09-25 16:06:06 +0800

    Sentinel整合nacos

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/springcloud/controller/RateLimitController.java
@@ -19,19 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RateLimitController {
 
-    @GetMapping("/byResource")
-    @SentinelResource(value = "byResource",blockHandler = "byResourceHandler")
+    @GetMapping("/byResource1")
+    @SentinelResource(value = "byResource1",blockHandler = "byResourceHandler")
     public CommonResult byResource(@RequestParam(value = "p1",required = false)String p1,
                         @RequestParam(value = "p2",required = false)String p2){
         return new CommonResult(200,"按资源名称测试", new Payment(2020L,"serial001"));
     }
 
-    public CommonResult byResourceHandler(BlockException e){
+    public CommonResult byResourceHandler(String p1,String p2,BlockException e){
         return new CommonResult(500,e.getClass().getCanonicalName()+"\t 服务不可用s");
     }
 
     @GetMapping("/customer")
-    @SentinelResource(value = "byResource",blockHandlerClass = CustomerBlockHandler.class, blockHandler = "ResourceHandler1")
+    @SentinelResource(value = "customer",blockHandlerClass = CustomerBlockHandler.class, blockHandler = "ResourceHandler1")
     public CommonResult customer(@RequestParam(value = "p1",required = false)String p1,
                                    @RequestParam(value = "p2",required = false)String p2){
         return new CommonResult(200,"按资源名称测试", new Payment(2020L,"serial001"));
