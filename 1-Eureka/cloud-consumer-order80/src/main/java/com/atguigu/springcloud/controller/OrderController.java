d13a5a94ca72fc3970eb38120c1dccc23582747c
PengYD <pengyd@178542>
2023-04-01 16:36:09 +0800

    LoadBalancer 自定义负载均衡算法

--- 1-Eureka/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ 1-Eureka/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,10 +2,15 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.lb.LoadBalancer;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -25,8 +30,24 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @PostMapping("/payment/create")
     public CommonResult<Integer> create(@RequestBody Payment payment) {
+
+        // 自定义负载均衡算法
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (!CollectionUtils.isEmpty(instances)) {
+            ServiceInstance instance = loadBalancer.instances(instances);
+            URI uri = instance.getUri();
+            restTemplate.postForObject(uri, payment, CommonResult.class);
+
+        }
+
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
